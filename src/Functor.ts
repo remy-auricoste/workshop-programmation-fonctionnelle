1109c5804fb40dc37308c37acf97e9a0e2981b38
Rémy Auricoste <remy.auricoste@gmail.com>
2023-08-23 11:22:04 +0200

    first draft

---
+++ src/Functor.ts
@@ -0,0 +1,47 @@
+export abstract class Functor<T> {
+  map: <B>(f: (a: T) => B) => Functor<B>;
+  /**
+   * Les 2 propositions suivantes doivent être vraies :
+   *      Identité invariante : pour tout fx, fx.map(x => x) = fx
+   *      composition : pour tout fx, fx.map(x => g(f(x))) = fx.map(f).map(g)
+   */
+}
+
+export abstract class Monad<T> {
+  map: <B>(f: (a: T) => B) => Monad<B>;
+
+  static of: <T>(a: T) => Monad<T>; // constructor static
+  flatMap: <B>(f: (item: T) => Monad<B>) => Monad<B>;
+  /**
+   * Les 3 propositions suivantes doivent être vraies :
+   *      identité à gauche : pour tout mx: Monad<T>, mx.flatMap(x => Monad.of(x)) = mx
+   *      identité à droite : pour tout x, Monad.of(x).flatMap(x => f(x)) = f(x)
+   *      associativité : pout tout mx, f, g , mx.flatMap(f).flatMap(g) = mx.flatMap(x => f(x).flatMap(g) )
+   */
+
+  static flatten<T>(level2: Monad<Monad<T>>): Monad<T> {
+    return level2.flatMap((x) => x);
+  }
+  static all<T>(monads: Monad<T>[]): Monad<T[]> {
+    const firstMonad = monads.at(0);
+    if (!firstMonad) {
+      return Monad.of<T[]>([]);
+    }
+    return monads.slice(1).reduce(
+      (acc, monad) =>
+        acc.flatMap((items) => monad.map<T[]>((item) => [...items, item])),
+      firstMonad.map((x) => [x])
+    );
+  }
+}
+
+/**
+ * Avantages :
+ *      fonctions pures => moins d'erreur
+ *      composabilité
+ * Inconvénients :
+ *      performances à peu près équivalentes, légèrement plus lent
+ *      beaucoup d'utilisation de la mémoire
+ *
+ * Implémentation TS : https://github.com/gcanti/fp-ts
+ */
